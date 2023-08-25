c440ae472850cadb4c9a7b4115cf122225f77d88
defAutumn <chromweechannelusa@gmail.com>
2023-08-25 08:11:30 +0300

    test

--- app/build.sh
+++ app/build.sh
@@ -2,7 +2,7 @@
 
 # Build the project
 echo "Building the project..."
-python3.11 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 export POSTGRES_HOST=containers-us-west-169.railway.app
 export POSTGRES_PORT=6048
@@ -12,8 +12,8 @@ export POSTGRES_PASSWORD=JOTH5KG5x3F8WnpjbZRl
 export DATABASE_URL=postgresql://postgres:JOTH5KG5x3F8WnpjbZRl@containers-us-west-169.railway.app:6048/railway
 
 echo "Make Migration..."
-python3.11 manage.py makemigrations --noinput
-python3.11 manage.py migrate --noinput
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.11 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
